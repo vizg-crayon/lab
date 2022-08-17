27df1f4d668ab0a583e8e1b49f40895d5673d746
vizg-crayon <vizrendra.g@crayon.com>
2022-08-17 16:05:29 +0800

    adding network and destroy

--- main.tf
+++ main.tf
@@ -17,4 +17,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "vizg-rg"
   location = "southeastasia"
+
+ tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+ }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "southeastasia"
+  resource_group_name = azurerm_resource_group.rg.name
 }
