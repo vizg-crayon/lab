9dfd0ff24adf51fc436afe99c685e70bd2c9d6d4
vizg-crayon <vizrendra.g@crayon.com>
2022-08-29 15:06:12 +0800

    update main.tf
    Add storage account and container

--- main.tf
+++ main.tf
@@ -8,12 +8,22 @@ terraform {
   }
 
   required_version = ">= 1.1.0"
+
+# Create a Terraform configuration with a backend configuration bloc
+    backend "azurerm" {
+        resource_group_name  = "vizg-rg"
+        storage_account_name = "vizstorageterraform"
+        container_name       = "vizcontainer"
+        key                  = "terraform.tfstate"
+    }
+
 }
 
 provider "azurerm" {
   features {}
 }
 
+# Create resources group
 resource "azurerm_resource_group" "rg" {
   name     = "vizg-rg"
   location = "southeastasia"
@@ -30,4 +40,37 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "southeastasia"
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "staging"
+    Team        = "DevOps"
+  }
+}
+
+# Create Azure storage 
+resource "azurerm_storage_account" "rg" {
+  name                     = "vizstorageterraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+    Team        = "DevOps"
+  }
+
+}
+
+# Create Container in azure storage
+resource "azurerm_storage_container" "rg" {
+  name                  = "vizcontainer"
+  storage_account_name  = azurerm_storage_account.rg.name
+  container_access_type = "blob"
+
+  tags = {
+    environment = "staging"
+    Team        = "DevOps"
+  }
+
 }
